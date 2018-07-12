4c3f428ef576fde2e712df46d4253cad776aab8f
Jiang Pierre <pierre-jiang@sfr.fr>
2018-07-12 15:49:26 +0800

    Beginning of the script for loading image and test function

--- Test/test.js
+++ Test/test.js
@@ -1,13 +1,12 @@
-import * as animation from "../animation.js"
+import {previous_state} from '../animation.js'
 
-
-function test__find_previous_state(){
+/*function test__previous_state(){
 	print("Test for find__previous_state...");
 	var counter_test = {Value = 1};
 	print("Test for counter = 1, expected result : 0");
-	print("Result obtained :" %d, animation.find__previous_state(counter_test));
+	print("Result obtained :" %d, animation.previous_state(counter_test));
 	alert(counter_test);
-}
+}*/
 
 function hello(){
 	alert("Hello World");
--- animation.js
+++ animation.js
@@ -1,18 +1,41 @@
 var MAX_FRAME = 10;
 var counter = {Value: 0};
 
-function find__previous_state(counter){
+
+/* ------------------- Function for loading frames for the animation ------------------- */
+
+//Reduce the value of counter by 1 if it's > 0, used
+function previous_state(counter){
 	if(counter.Value > 0)
 		counter.Value--;
 }
 
-function find__next_state(counter){
+function next_state(counter){
 	if(counter.Value < MAX)
 		counter.Value++;
 }
 
-function load_frame(counter){
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
 
 }
 
-export default find__previous_state;
+/*function load_frame(counter){
+	show_image(+counter+".png")
+}*/
+
+/*--------------- Test function-------------------*/
+
+function test__previous_state(){
+	var counter_test = {Value: 1};
+	previous_state(counter_test);
+	alert("Testing previous_state function...\n Entering value : 1, expected : 0, obtained :", counter_test.Value);
+}
+
+function hello(){
+	alert("Hello World");
+}
